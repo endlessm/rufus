bf738dece9779f81dcd810a1cead47c85a8eb602
Radu Ancuta <radu.ancuta@movial.com>
2016-09-09 16:16:51 +0300

    Increased version to 1.0.2.5 for internal release.

--- src/endless/Version.h
+++ src/endless/Version.h
@@ -3,7 +3,7 @@
 #define RELEASE_VER_MAIN    1 
 #define RELEASE_VER_MAIN2   0
 #define RELEASE_VER_SUB     2
-#define RELEASE_VER_SUB2    4
+#define RELEASE_VER_SUB2    5
 
 // version number (string)
 #define TOSTRING2(arg) #arg
