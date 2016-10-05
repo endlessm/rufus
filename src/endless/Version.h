eff61818428a891e9fdfc19c392fa660e03f3230
Robert McQueen <rob@endlessm.com>
2016-10-05 18:55:49 +0100

    Version 1.0.2.12

--- src/endless/Version.h
+++ src/endless/Version.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define RELEASE_VER_MAIN    1 
+#define RELEASE_VER_MAIN    1
 #define RELEASE_VER_MAIN2   0
 #define RELEASE_VER_SUB     2
-#define RELEASE_VER_SUB2    11
+#define RELEASE_VER_SUB2    12
 
 // version number (string)
 #define TOSTRING2(arg) #arg
