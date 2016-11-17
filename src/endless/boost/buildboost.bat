596b5ba1fb39459c37f0c8d50731a4fe3e0c2749
Radu Ancuta <radu.ancuta@movial.com>
2016-11-17 16:19:22 +0200

    Remove boost from build and use installed boost binaries.

--- src/endless/boost/buildboost.bat
+++
@@ -1,9 +0,0 @@
-set stagedir=%1
-set builddir=%2
-set buildflavor=%3
-set nrproc=%4
-
-set commandtorun=b2.exe  -j%nrproc% --stagedir=%stagedir% --build-dir=%builddir% architecture=x86  address-model=32 --toolset=msvc --build-type=minimal variant=%buildflavor% link=static threading=multi runtime-link=static stage
-
-%commandtorun% > nul
-%commandtorun%
