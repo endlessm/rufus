63e8e41789acd9a195ce4217dd8bae0c1d09c85d
Will Thompson <wjt@endlessm.com>
2017-02-16 14:39:50 +0000

    Link static 7z library

--- src/7zip-cpp/7zpp/SevenZipLibrary.h
+++ src/7zip-cpp/7zpp/SevenZipLibrary.h
@@ -7,20 +7,12 @@ namespace SevenZip
 {
 	class SevenZipLibrary
 	{
-	private:
-
-		typedef UINT32 (WINAPI * CreateObjectFunc)( const GUID* clsID, const GUID* interfaceID, void** outObject );
-
-		HMODULE				m_dll;
-		CreateObjectFunc	m_func;
-
 	public:
 
 		SevenZipLibrary();
 		~SevenZipLibrary();
 
 		bool Load();
-		bool Load( const TString& libraryPath );
 		void Free();
 
 		bool CreateObject( const GUID& clsID, const GUID& interfaceID, void** outObject ) const;
