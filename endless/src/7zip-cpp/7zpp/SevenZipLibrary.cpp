f9625cbc20d08f2855c8388b2217203328500dc1
Will Thompson <wjt@endlessm.com>
2017-02-16 14:39:50 +0000

    Link static 7z library

--- endless/src/7zip-cpp/7zpp/SevenZipLibrary.cpp
+++ endless/src/7zip-cpp/7zpp/SevenZipLibrary.cpp
@@ -2,15 +2,16 @@
 #include "SevenZipLibrary.h"
 #include "GUIDs.h"
 
+// This is the entrypoint to 7z, defined in
+// src/7z/CPP/7zip/Archive/DllExports2.cpp. As the name suggests, it's normally
+// exported as a DLL symbol and looked up with GetProcAddress(), so does not
+// appear in 7z's headers.
+STDAPI CreateObject(const GUID *clsid, const GUID *iid, void **outObject);
 
 namespace SevenZip
 {
 
-const TString DefaultLibraryPath = _T( "7z.dll" );
-
 SevenZipLibrary::SevenZipLibrary()
-	: m_dll( NULL )
-	, m_func( NULL )
 {
 }
 
@@ -21,48 +22,16 @@ SevenZipLibrary::~SevenZipLibrary()
 
 bool SevenZipLibrary::Load()
 {
-	return Load( DefaultLibraryPath );
-}
-
-bool SevenZipLibrary::Load( const TString& libraryPath )
-{
-	Free();
-	m_dll = LoadLibrary( libraryPath.c_str() );
-	if ( m_dll == NULL )
-	{
-		return false;
-		//throw SevenZipException( GetWinErrMsg( _T( "LoadLibrary" ), GetLastError() ) );
-	}
-
-	m_func = reinterpret_cast< CreateObjectFunc >( GetProcAddress( m_dll, "CreateObject" ) );
-	if ( m_func == NULL )
-	{
-		Free();
-		return false;
-		//throw SevenZipException( _T( "Loaded library is missing required CreateObject function" ) );
-	}
 	return true;
 }
 
 void SevenZipLibrary::Free()
 {
-	if ( m_dll != NULL )
-	{
-		FreeLibrary( m_dll );
-		m_dll = NULL;
-		m_func = NULL;
-	}
 }
 
 bool SevenZipLibrary::CreateObject( const GUID& clsID, const GUID& interfaceID, void** outObject ) const
 {
-	if ( m_func == NULL )
-	{
-		return false;
-		//throw SevenZipException( _T( "Library is not loaded" ) );
-	}
-
-	HRESULT hr = m_func( &clsID, &interfaceID, outObject );
+	HRESULT hr = ::CreateObject( &clsID, &interfaceID, outObject );
 	if ( FAILED( hr ) )
 	{
 		return false;
